134f0df00a1cf0405f89b42c289295ecc908d661
yeonlisa <tkzw4869@gmail.com>
2022-02-23 15:57:37 +0900

    Classes

--- public/app.js
+++ public/app.js
@@ -1,12 +1,24 @@
 "use strict";
-// const anchor = document.querySelector('a')!;
-// if(anchor) {
-//     console.log(anchor.href);
-// }
-// console.log(anchor.href);
-//const form = document.querySelector('form')!;
+// classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return "".concat(this.client, " owes \u00A3").concat(this.amount, " for ").concat(this.details);
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('mario', 'work on the mario website', 250);
+var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
 var form = document.querySelector('.new-item-form');
-// console.log(form.children);
+console.log(form.children);
 // inputs
 var type = document.querySelector('#type');
 var tofrom = document.querySelector('#tofrom');
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,32 @@
-// const anchor = document.querySelector('a')!;
-// if(anchor) {
-//     console.log(anchor.href);
-// }
-// console.log(anchor.href);
+// classes
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
 
-//const form = document.querySelector('form')!;
+    constructor(c: string, d: string, a: number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne)
+invoices.push(invTwo);
+
+console.log(invoices);
+
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-// console.log(form.children);
+console.log(form.children);
 
 // inputs
 const type = document.querySelector('#type') as HTMLInputElement;
