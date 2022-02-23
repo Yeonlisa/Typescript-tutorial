9a6fd08d4551eea9588021a4f4d21a802d135668
yeonlisa <tkzw4869@gmail.com>
2022-02-23 16:25:53 +0900

    Interfaces

--- public/app.js
+++ public/app.js
@@ -1,12 +1,19 @@
-import { Invoice } from './classes/Invoice.js';
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());
-});
+const me = {
+    name: 'shaun',
+    //age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent ', amount);
+        return amount;
+    },
+};
+const greetPerson = (person) => {
+    console.log('hello ', person.name);
+};
+greetPerson(me);
+console.log(me);
 const form = document.querySelector('.new-item-form');
 console.log(form.children);
 // inputs
@@ -18,3 +25,4 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
+export {};
