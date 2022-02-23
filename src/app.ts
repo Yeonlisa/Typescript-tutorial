56bf3b6830c66a5f9f5b5a1bf10fdb60ff8727ba
yeonlisa <tkzw4869@gmail.com>
2022-02-23 17:14:54 +0900

    Enums

--- src/app.ts
+++ src/app.ts
@@ -29,34 +29,26 @@ form.addEventListener('submit', (e: Event) => {
     list.render(doc, type.value, 'end');
 });
 
-// GENERICS
+// ENUMS
 
-const addUID = <T extends {name: string}>(obj: T) => {
-    let uid = Math.floor(Math.random() * 100);
-    return {...obj, uid};
-}
+enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR };
 
-let docOne = addUID({name: 'yoshi', age: 40});
-
-console.log(docOne.name);
-
-// with interfaces
 interface Resource<T> {
     uid: number;
-    resourceName: string;
+    resourceType: ResourceType;
     data: T;
 }
 
-const docThree: Resource<object> = {
-    uid: 1, 
-    resourceName: 'person', 
-    data: { name: 'shaun' }
-};
+const docOne: Resource<object> = {
+    uid: 1,
+    resourceType: ResourceType.BOOK,
+    data: { title: 'name of the wind' }
+}
+const docTwo: Resource<object> = {
+    uid: 10,
+    resourceType: ResourceType.DIRECTOR,
+    data: { title: 'name of the wind' }
+}
 
-const docFour: Resource<string[]> = {
-    uid: 2, 
-    resourceName: 'shoppingList', 
-    data: ['bread', 'milk']
-};
-
-console.log(docThree, docFour);
+console.log(docOne);
+console.log(docTwo);
